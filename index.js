b3f764cf8bf6f4dfc8a5e5edd76868a135324458
priyankayadav98 <Priyanka.yadav@mca.christuniversity.in>
2023-05-25 11:27:20 +0530

    new change

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
+
+function newalert() { 
 let newvar = document.getElementById("fname")
 console.log(newvar)
 let lastvar = document.getElementById("lname")
 console.log(lastvar)
-
-let final_alert = newvar + "" + lastvar
-
-function newalert()
-{
-    console.log(final_alert)
+    alert("Full name: " + newvar + " " + lastvar)
+    return
 }
