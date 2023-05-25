880a8b03d8177f1ae3ff33663059af4de5651854
priyankayadav98 <Priyanka.yadav@mca.christuniversity.in>
2023-05-25 11:09:28 +0530

    Add project files.

---
+++ index.js
@@ -0,0 +1,11 @@
+let newvar = document.getElementById("fname")
+console.log(newvar)
+let lastvar = document.getElementById("lname")
+console.log(lastvar)
+
+let final_alert = newvar + "" + lastvar
+
+function newalert()
+{
+    console.log(final_alert)
+}
