f0693ce49a1638c4cdf7bc306dadd66878e1947e
priyankayadav98 <Priyanka.yadav@mca.christuniversity.in>
2023-05-26 13:26:21 +0530

    changes  to context and menu

--- sample-project/src/Components/Menu-item.tsx
+++ sample-project/src/Components/Menu-item.tsx
@@ -1,14 +1,20 @@
 import { MouseEvent } from "react";
 function Menu_item() {
   const items: Array<string> = ["java", "C", "python"];
-  const handleClick = (event: MouseEvent) => console.log(event);
+  const handleClick = (index: number, event: MouseEvent) => {
+    console.log(index + " " + event.type);
+  };
   return (
     <>
       <div className="w-25 p-3">
-        {items.length === 0 ? "No items" : null}
+        {items.length === 0 && <p>No Items is found in the list</p>}
         <ul className="list-group">
           {items.map((x, index) => (
-            <li className="list-group-item " key={x} onClick={handleClick}>
+            <li
+              className="list-group-item "
+              key={x}
+              onClick={(event) => handleClick(index, event)}
+            >
               {x}
             </li>
           ))}
