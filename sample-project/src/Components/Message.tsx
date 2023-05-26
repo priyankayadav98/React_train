031f083ee1f8558b1339084abc3a409e4030c1f3
priyankayadav98 <Priyanka.yadav@mca.christuniversity.in>
2023-05-26 15:48:23 +0530

    changes to props

--- sample-project/src/Components/Message.tsx
+++ sample-project/src/Components/Message.tsx
@@ -2,10 +2,10 @@ interface Props {
   course: number;
 }
 
-function Message(props: Props) {
+function Message({ course }: Props) {
   return (
     <div className="text-center">
-      <h1 className="text-info">Welcome to the {props.course} on My-app!</h1>
+      <h1 className="text-info">Welcome to the {course} on My-app!</h1>
     </div>
   );
 }
